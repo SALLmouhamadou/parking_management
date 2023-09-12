01291e7eb6373cfecf8634699573fc5d7f11a5b5
SALLmouhamadou <salllmouha10@gmail.com>
2023-09-12 19:02:22 +0200

    Ajoute du page card.html

--- src/main/java/com/parking_managment/controller/CardController.java
+++ src/main/java/com/parking_managment/controller/CardController.java
@@ -1,4 +1,4 @@
-package com.parking_managment.restController;
+package com.parking_managment.controller;
 
 import com.parking_managment.entity.Car;
 import com.parking_managment.entity.Card;
@@ -6,25 +6,33 @@ import com.parking_managment.entity.Person;
 import com.parking_managment.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class CardRestController {
+public class CardController {
     @Autowired
     CardService cardService;
 
-    @PostMapping("/api/add/card")
-    public String addCar(@RequestBody Card card, @RequestBody Person person, @RequestBody Car car){
+    @PostMapping("/card/add")
+    public String addCar(@RequestAttribute Card card, @RequestAttribute Person person, @RequestAttribute Car car){
         List<Car>  myCars = new ArrayList<Car>();
         myCars.add(car);
         person.setCars(myCars);
         card.setPerson(person);
         cardService.addCard(card);
-        return "Votre carte de parking à bien été enregistrer";
+        return "card";
     }
-
+    @GetMapping("/card/save")
+    public ModelAndView showAddCard(ModelAndView mav){
+        mav.addObject("card", new Card());
+        mav.setViewName("card");
+        return mav;
+    }
 }
